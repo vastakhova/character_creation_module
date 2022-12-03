0df6a3c5ac6a75cb5a9b76b680d91cfb2d8af760
Valeriia Astakhova <vastakhova7693@yandex.ru>
2022-12-03 23:06:08 +0100

    Добавлены докстрингс и модульgraphic_arts

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from random import randint
 
+from graphic_arts.start_game_banner import run_screensaver
+
 
 def attack(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
@@ -78,7 +80,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
